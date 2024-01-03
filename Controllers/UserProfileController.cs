185738f53a4f2b36797b7caf9323d9eae2e5bb0e
Ryan Ramsey <rramsey1115@gmail.com>
2024-01-03 14:45:33 -0600

    promote and demote log to console

--- Controllers/UserProfileController.cs
+++ Controllers/UserProfileController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using BiancasBikes.Data;
 using BiancasBikes.Models.DTOs;
 using Microsoft.EntityFrameworkCore;
+using BiancasBikes.Models;
 
 namespace BiancasBikes.Controllers;
 
@@ -34,6 +35,29 @@ public class UserProfileController : ControllerBase
                 Email = up.IdentityUser.Email,
                 UserName = up.IdentityUser.UserName
             })
-            .ToList());
+        .ToList());
     }
+
+
+    [HttpGet("withroles")]
+    [Authorize(Roles = "Admin")]
+    public IActionResult GetWithRoles()
+    {
+        return Ok(_dbContext.UserProfiles
+        .Include(up => up.IdentityUser)
+        .Select(up => new UserProfileDTO
+        {
+            Id = up.Id,
+            FirstName = up.FirstName,
+            LastName = up.LastName,
+            Address = up.Address,
+            Email = up.IdentityUser.Email,
+            UserName = up.IdentityUser.UserName,
+            IdentityUserId = up.IdentityUserId,
+            Roles = _dbContext.UserRoles
+            .Where(ur => ur.UserId == up.IdentityUserId)
+            .Select(ur => _dbContext.Roles.SingleOrDefault(r => r.Id == ur.RoleId).Name)
+            .ToList()
+        }));
+}
 }
--- client/src/managers/userProfileManager.js
+++ client/src/managers/userProfileManager.js
@@ -4,3 +4,6 @@ export const getUserProfiles = () => {
   return fetch(_apiUrl).then((res) => res.json());
 };
 
+export const getUserProfilesWithRoles = () => {
+    return fetch(_apiUrl + "/withroles").then((res) => res.json());
+};
